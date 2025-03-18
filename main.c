505d505bfc9328d86b783aeff03596ad78728a5c
marc55s <marc.schoenig@gmail.com>
2025-03-18 23:20:18 +0100

    refactor(main): update point calculations to be relative to camera
    - Refactored code for better readability and maintainability
    - Changed point calculations to be relative to the camera instead of world coordinates
    - Updated related logic to ensure correct positioning and transformations

--- main.c
+++ main.c
@@ -4,116 +4,93 @@
 
 #define WIDTH 1200
 #define HEIGHT 800
-#define GRID_WHITE (Color){120,120,120,255}
+#define GRID_WHITE (Color){120, 120, 120, 255}
 
+double sinus(double x) { return sin(x); }
 
-double SCALE = 50.0; // Scale factor (pixels per unit)
-
-double function(double x) { return sin(x); }
-double function2(double x) { return x; }
-double quadratic(double x) { return x * x; }
-double cool(double x) { return tan(x); }
-
-void DrawAxes(Camera2D cam) {
+void DrawGraph(double (*func)(double), Camera2D cam) {
     Vector2 worldMin = GetScreenToWorld2D((Vector2){0, 0}, cam);
     Vector2 worldMax = GetScreenToWorld2D((Vector2){WIDTH, HEIGHT}, cam);
 
-    DrawLine(worldMin.x, 0, worldMax.x, 0, RED);  // X-axis
-    DrawLine(0, worldMin.y, 0, worldMax.y, BLUE); // Y-axis
+    double DELTA_X = (fabs(worldMin.x - worldMax.x)) / 5000; // guarantees same amount of steps, for any width of x-axis
 
-    DrawText("X", worldMax.x-20,10,20,RAYWHITE);
-    DrawText("Y", -20,worldMin.y+10,20,RAYWHITE);
-}
+    for (double i = worldMin.x; i < worldMax.x; i += DELTA_X) {
+        double y = func(i);
+        double y2 = func(i+DELTA_X);
 
-void DrawGrid2D(int slices, int spacing, Camera2D cam) {
-    Vector2 worldMin = GetScreenToWorld2D((Vector2){0, 0}, cam);
-    Vector2 worldMax = GetScreenToWorld2D((Vector2){WIDTH, HEIGHT}, cam);
-    for (int i = -slices; i <= slices; i++) {
-        DrawLine(spacing*i, worldMin.y, spacing*i, worldMax.y, GRID_WHITE);
-        DrawLine(worldMin.x, spacing*i, worldMax.x, spacing*i, GRID_WHITE);  // X-axis
-    }
-
-}
-
-void DrawGraph(double (*f)(double), Color color) {
-
-    double delta_x = WIDTH / 2 / SCALE / 500;
-
-    Vector2 points[10000];
-
-    int index = 0;
-
-    for (double x = -WIDTH / 2 / SCALE; x < WIDTH / 2 / SCALE; x += delta_x) {
-
-        double y1 = f(x);
-        double y2 = f(x + delta_x);
-
-        if(fabs(y1-y2) < 100){
-            Vector2 p1 = {x, -y1};
-            Vector2 p2 = {(x + delta_x), -y2};
-            p1 = Vector2Scale(p1, SCALE);
-            p2 = Vector2Scale(p2, SCALE);
-            points[index++] = p1;
-            points[index++] = p2;
-            DrawLineV(p1,p2,color);
-        }else{
-            TraceLog(LOG_INFO,"DIFF: (%lf)",fabs(y1-y2));
+        if(fabs(cos(i)) > 0.01 && fabs(y-y2) < 100) {
+            Vector2 start = (Vector2) {i,-y};
+            Vector2 end = (Vector2) {i+DELTA_X,-y2};
+            DrawLineV(start,end,RAYWHITE);
         }
 
-
     }
 }
 
-void MoveCam(Camera2D *cam) {
-    double step = 3 * cam->zoom;
+void DrawAxes(Camera2D camera) {
+    Vector2 worldMin = GetScreenToWorld2D((Vector2){0, 0}, camera);
+    Vector2 worldMax = GetScreenToWorld2D((Vector2){WIDTH, HEIGHT}, camera); 
+
+    TraceLog(LOG_INFO,"worldMin: (%f, %f), worldMax: (%f, %f)\n", worldMin.x, worldMin.y, worldMax.x, worldMax.y);
+    DrawLine(worldMin.x-1, 0, worldMax.x+1, 0, GOLD); // X-Axis
+    DrawLine(0, worldMin.y-1, 0, worldMax.y+1, GOLD); // Y-Axis
+}
+
+void UpdateCameraControl(Camera2D *camera) {
+    Vector2 worldBeforeZoom  = GetScreenToWorld2D((Vector2){ WIDTH / 2.0f, HEIGHT / 2.0f }, *camera);
+
+    TraceLog(LOG_INFO, "zoom at > %f",camera->zoom);
+    if (IsKeyDown(KEY_E)){
+        camera->zoom *= 1.1;
+    }
+    if (IsKeyDown(KEY_Q)){
+        camera->zoom /= 1.1;
+    }
+
+    Vector2 worldAfterZoom = GetScreenToWorld2D((Vector2){ WIDTH / 2.0f, HEIGHT / 2.0f }, *camera);
+
+    camera->target.x -= (worldAfterZoom.x - worldBeforeZoom.x);
+    camera->target.y -= (worldAfterZoom.y - worldBeforeZoom.y);
+
+    float step = 8;
+
     if (IsKeyDown(KEY_W)) {
-        cam->offset.y += step;
+        camera->offset.y += step;
     }
     if (IsKeyDown(KEY_A)) {
-        cam->offset.x += step;
+        camera->offset.x += step;
     }
     if (IsKeyDown(KEY_S)) {
-        cam->offset.y -= step;
+        camera->offset.y -= step;
     }
     if (IsKeyDown(KEY_D)) {
-        cam->offset.x -= step;
-    }
-    if (IsKeyDown(KEY_Q)) {
-        if (cam->zoom > 1)
-            cam->zoom = sqrt(cam->zoom);
-    }
-    if (IsKeyDown(KEY_E)) {
-        cam->zoom += 0.1;
+        camera->offset.x -= step;
     }
 }
 
 int main() {
     InitWindow(WIDTH, HEIGHT, "Function Plotter");
     SetTargetFPS(60);
-    Vector2 target = {0, 0};
+    SetGesturesEnabled(GESTURE_PINCH_IN | GESTURE_PINCH_OUT);
     Vector2 offset = {WIDTH / 2, HEIGHT / 2};
-    Camera2D cam = (Camera2D){offset, target, 0, 1};
+    Vector2 target = {0};
+    Camera2D camera = (Camera2D){offset, target, 0, 10};
 
     while (!WindowShouldClose()) {
+        UpdateCameraControl(&camera);
+
         BeginDrawing();
-        BeginMode2D(cam);
+        BeginMode2D(camera);
 
-        ClearBackground(DARKGRAY);
+        ClearBackground(BLACK);
 
-        MoveCam(&cam);
-        DrawGrid2D(10000,50,cam);
-        DrawAxes(cam);
-
-        DrawGraph(function,YELLOW);
-        DrawGraph(function2,DARKPURPLE);
-        DrawGraph(quadratic,RED);
-        DrawGraph(cool,RAYWHITE);
-
-        DrawCircle(0, 0, 5, PURPLE); // Center dot (0,0)
-
+        DrawAxes(camera);
+        DrawGraph(sinus, camera);
+        DrawGraph(cos, camera);
+        DrawGraph(tan, camera);
+        /*DrawCircle(0, 0, 10, RED);*/
 
         EndMode2D();
-
         EndDrawing();
     }
 
